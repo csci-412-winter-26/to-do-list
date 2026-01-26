cb62ee0e74550f34052a6a7ca089143b7dbc1ebd
Neo-Hao <qianghaoqiang@gmail.com>
2026-01-25 21:36:24 -0800

    update test cases

--- __tests__/components/InputArea.test.js
+++ __tests__/components/InputArea.test.js
@@ -1,25 +1,16 @@
-
 import { render, fireEvent } from '@testing-library/react-native';
 import InputArea from '../../components/InputArea';
 
 // test if the onPress function is called when the add button is pressed
 test('calls onPress function when add button is pressed', () => {
-  const task = 'Hello test';
-  const mockSetTask = jest.fn();
-  const mockHandleAddTask = jest.fn();
+  const mockSetTasks = jest.fn();
 
   const { getByPlaceholderText, getByTestId } = render(
-    <InputArea
-      task={task}
-      setTask={mockSetTask}
-      handleAddTask={mockHandleAddTask}
-    />
+    <InputArea setTasks={mockSetTasks} />,
   );
   const input = getByPlaceholderText('Enter task');
   fireEvent.changeText(input, 'New to-do');
-  expect(mockSetTask).toHaveBeenCalledWith('New to-do');
-
   const button = getByTestId('buttonAdd');
   fireEvent.press(button);
-  expect(mockHandleAddTask).toHaveBeenCalledWith();
+  expect(mockSetTasks).toHaveBeenCalledWith(expect.any(Function));
 });
